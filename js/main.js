0a2b8a660c511c8933605bb0dd624c9c68ea859f
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 10:58:59 +0200

    refine

--- js/main.js
+++ js/main.js
@@ -20,18 +20,18 @@ console.log("Landing");
 
     menuitems.forEach(menuitem => {
         var Menubar = document.querySelector("menu");
-        console.log(Menubar);
-        console.log(menuitem);
 
         const newMenitem = document.createElement("Menuitem");
-        newMenitem.innerHTML = menuitem.name;
+        newMenitem.innerHTML = menuitem.Name;
         newMenitem.href = menuitem.link;
 
-        newMenitem.classList.add("Test");
+        newMenitem.classList.add("Menuitem");
+
+        //TODO: Isactive check
 
         Menubar.appendChild(newMenitem);
 
-        //TODO: Isactive
+        
 
         
         
