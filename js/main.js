0a8ba644386b447d4c2fb26a0e9d4348c3fa04a0
NochEinMaloon <ein.marlon.oder.das.malon@gmail.com>
2024-09-13 10:34:06 +0200

    menuitem function

--- js/main.js
+++ js/main.js
@@ -3,9 +3,28 @@ console.log("Landing");
 /* class Menu{
     constructor()
 } */
+
+
+    function createELM(name, parent, id){
+
+    }
+
+    const Menubar = document.querySelector("menu");
+
     console.log(menuitems);
     
+
     menuitems.forEach(menuitem => {
+        const newMenitem = document.createElement("Menuitem");
+        newMenitem.innerHTML = menuitem.name;
+        newMenitem.href = menuitem.link;
+
+        newMenitem.classList.add("Test");
+
+        Menubar.appendChild(newMenitem);
+
+        //TODO: Isactive
+
         console.log(menuitem);
         
     });
